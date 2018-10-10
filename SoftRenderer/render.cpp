c2d719865e8a08e8dde5513b7c5e729c1d49ce6f
Viktor <ctxx61@gmail.com>
2018-10-10 16:41:24 +0800

    Change additional library to make it work in Realse Mode

--- SoftRenderer/render.cpp
+++ SoftRenderer/render.cpp
@@ -118,7 +118,10 @@ void Texture::Load(const char* filename)
 Vector4i Texture::Map(float tu, float tv)
 {
 	Vector4i result;
-	result = { 0,0,0,0 };
+	result.x = 0;
+	result.y = 0;
+	result.z = 0;
+	result.w = 0;
 	if (buf.empty()) return result;
 	int u = (int)(tu*width)%width;
 	int v = (int)(tv*width)%height;
@@ -128,7 +131,11 @@ Vector4i Texture::Map(float tu, float tv)
 	v = v >= 0 ? v : -v;
 
 	cv::Vec3b tex_w = buf.at<cv::Vec3b>(v, u);
-	result = { tex_w[2],tex_w[1],tex_w[0],0 };
+	result.x = tex_w[2];
+	result.y = tex_w[1];
+	result.z = tex_w[0];
+	result.w = 0;
+	//result = { tex_w[2],tex_w[1],tex_w[0],0 };
 	return result;
 }
 
